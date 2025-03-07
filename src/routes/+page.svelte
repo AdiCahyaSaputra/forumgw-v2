c4c1be2030a24e1056487b5e1bc3c92dc44c93fe
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2025-03-07 14:10:30 +0700

    test: add button from shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1 class="text-2xl font-bold text-red-600">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="p-4">
+	<h1 class="text-2xl font-bold text-red-600">Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+	<Button class="mt-4" variant="outline">Hello</Button>
+</div>
