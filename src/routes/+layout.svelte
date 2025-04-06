6cadf52a6a670478dabf570d6ddfc96880407311
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2025-04-06 13:45:19 +0700

    style: add progressbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,12 @@
 	import { Toaster } from '$lib/components/ui/sonner/index';
 	import { QueryClientProvider } from '@tanstack/svelte-query';
 	import type { LayoutProps } from './$types';
+	import { ProgressBar } from "@prgm/sveltekit-progress-bar";
 
 	let { data, children }: LayoutProps = $props();
 </script>
+
+<ProgressBar class="text-red-500" zIndex={999} />
 
 <ParaglideJS {i18n}>
 	<QueryClientProvider client={data.queryClient}>
